2df710b95096ac32a0851f3fcb000de503cb3bf8
superstan777 <viir4d@gmail.com>
2025-03-19 14:19:54 +0100

    Move databse to external file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,19 +2,17 @@ import { useEffect, useState } from "react";
 import { AppState, AppStateStatus } from "react-native";
 import { Stack, router } from "expo-router";
 import Button from "@/components/Button";
-import { SQLiteProvider, SQLiteDatabase } from "expo-sqlite";
+import { SQLiteProvider } from "expo-sqlite";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useStore } from "@/utility/store";
+import { DB_NAME, createDbIfNeeded } from "@/utility/db";
 
 export default function RootLayout() {
   const [appState, setAppState] = useState<AppStateStatus>(
     AppState.currentState
   );
 
-  const { setCurrentDate, currentDate } = useStore();
-
-  console.log(appState);
-  console.log(currentDate);
+  const { setCurrentDate } = useStore();
 
   useEffect(() => {
     const handleAppStateChange = (nextAppState: AppStateStatus) => {
@@ -34,41 +32,9 @@ export default function RootLayout() {
     };
   }, [appState, setCurrentDate]);
 
-  const createDbIfNeeded = async (db: SQLiteDatabase) => {
-    try {
-      await db.execAsync("PRAGMA foreign_keys = ON;");
-
-      await db.execAsync(
-        `CREATE TABLE IF NOT EXISTS habits (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          name TEXT NOT NULL,
-          days TEXT NOT NULL,
-          created_at INTEGER
-        );`
-      );
-
-      await db.execAsync(
-        `CREATE TABLE IF NOT EXISTS habit_events (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          habit_id INTEGER NOT NULL,
-          date TEXT NOT NULL,
-          completed_at INTEGER DEFAULT NULL,
-          FOREIGN KEY (habit_id) REFERENCES habits(id) ON DELETE CASCADE
-        );`
-      );
-
-      console.log("Database loaded");
-    } catch (error) {
-      console.error("Error creating database:", error);
-    }
-  };
-
   return (
     <GestureHandlerRootView>
-      <SQLiteProvider
-        databaseName="test1111111111.db"
-        onInit={createDbIfNeeded}
-      >
+      <SQLiteProvider databaseName={DB_NAME} onInit={createDbIfNeeded}>
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen
@@ -88,7 +54,6 @@ export default function RootLayout() {
               ),
             }}
           />
-
           <Stack.Screen
             name="[habitId]"
             options={{
--- utility/db.ts
+++ utility/db.ts
@@ -1,29 +1,33 @@
-// import * as SQLite from 'expo-sqlite';
+import { SQLiteDatabase } from "expo-sqlite";
 
-// // Open or create the database
-// const db = SQLite.openDatabaseAsync('habitTracker.db');
+// export const DB_NAME = "habit_tracker.db";
+export const DB_NAME = "test1111111111.db";
 
-// // Initialize the database with required tables
-// const initDatabase = () => {
-//   return new Promise((resolve, reject) => {
-//     db.transaction(tx => {
-//       // Create habits table
-//       tx.executeSql(
-//         'CREATE TABLE IF NOT EXISTS habits (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, created_at INTEGER);',
-//         [],
-//         () => {},
-//         (_, error) => { console.error('Error creating habits table:', error); reject(error); return false; }
-//       );
+export const createDbIfNeeded = async (db: SQLiteDatabase) => {
+  try {
+    await db.execAsync("PRAGMA foreign_keys = ON;");
 
-//       // Create habit_events table
-//       tx.executeSql(
-//         'CREATE TABLE IF NOT EXISTS habit_events (id INTEGER PRIMARY KEY AUTOINCREMENT, habit_id INTEGER, date TEXT, completed INTEGER DEFAULT 0, FOREIGN KEY (habit_id) REFERENCES habits (id) ON DELETE CASCADE, UNIQUE(habit_id, date));',
-//         [],
-//         () => { resolve(); },
-//         (_, error) => { console.error('Error creating habit_events table:', error); reject(error); return false; }
-//       );
-//     });
-//   });
-// };
+    await db.execAsync(
+      `CREATE TABLE IF NOT EXISTS habits (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        days TEXT NOT NULL,
+        created_at INTEGER
+      );`
+    );
 
-// export { db, initDatabase };
+    await db.execAsync(
+      `CREATE TABLE IF NOT EXISTS habit_events (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        habit_id INTEGER NOT NULL,
+        date TEXT NOT NULL,
+        completed_at INTEGER DEFAULT NULL,
+        FOREIGN KEY (habit_id) REFERENCES habits(id) ON DELETE CASCADE
+      );`
+    );
+
+    console.log("Database initialized successfully.");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+  }
+};
