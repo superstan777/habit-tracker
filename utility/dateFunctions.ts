8a6e27c158feaa56f960e38b5d61e608f5fad624
superstan777 <viir4d@gmail.com>
2025-03-26 19:15:01 +0100

    Update date functions

--- utility/dateFunctions.ts
+++ utility/dateFunctions.ts
@@ -3,3 +3,11 @@ export const getWeekNumber = (date: Date): number => {
   const diff = date.getTime() - oneJan.getTime();
   return Math.floor(diff / (7 * 24 * 60 * 60 * 1000)) + 1;
 };
+
+export const convertUTCToLocal = (utcDate: string): Date => {
+  const [year, month, day] = utcDate.split("-").map(Number);
+  const utcDateObj = new Date(Date.UTC(year, month - 1, day));
+  return new Date(
+    utcDateObj.getTime() + utcDateObj.getTimezoneOffset() * 60000
+  );
+};
